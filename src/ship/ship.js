d22db7feb3d69398433e7958e99489fdf5eeb8c5
James Olsen <olsenjames1116@gmail.com>
2023-02-13 16:43:50 -0700

    Ship class has all necessary properties

--- src/ship/ship.js
+++ src/ship/ship.js
@@ -1,3 +1,8 @@
 export default class Ship {
-
+    constructor(length, hits = 0, sunk = false, coordinates = null) {
+        this.length = length;
+        this.hits = hits;
+        this.sunk = sunk;
+        this.coordinates = coordinates;
+    }
 }
--- src/ship/ship.test.js
+++ src/ship/ship.test.js
@@ -1,7 +1,23 @@
 import Ship from './ship.js';
 
-const ship = new Ship();
+const ship = new Ship(2);
 
 test('Returns an object', () => {
-    expect(ship).toEqual({});
+    expect(typeof ship).toEqual('object');
+});
+
+test('Ship has length property', () => {
+    expect(ship.hasOwnProperty('length')).toBe(true);
+});
+
+test('Ship has hits property', () => {
+    expect(ship.hasOwnProperty('hits')).toBe(true);
+});
+
+test('Ship has sunk property', () => {
+    expect(ship.hasOwnProperty('sunk')).toBe(true);
+});
+
+test('Ship has coordinates property', () => {
+    expect(ship.hasOwnProperty('coordinates')).toBe(true);
 });
