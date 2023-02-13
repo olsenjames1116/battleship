ba54b278ab0a16a46b8594c66b9cecbe9fd3b7ee
James Olsen <olsenjames1116@gmail.com>
2023-02-13 09:47:56 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+mode: 'development',
+devServer: {
+    static: './dist',
+},
+devtool: 'inline-source-map',
+entry: './src/index.js',
+output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+},
+module: {
+    rules: [
+    {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+    },
+    {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ],
+},
+};
