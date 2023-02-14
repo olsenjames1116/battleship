1d66c397842c2cc6da3e2207437622c4783ce257
James Olsen <olsenjames1116@gmail.com>
2023-02-13 18:55:31 -0700

    Add buildBoard method to Gameboard object
    This method builds a 2D array to represent each individual square on the
    board. Each square is created as an instance of the Square object to
    keep track of the game.

--- src/gameboard/gameboard.js
+++ src/gameboard/gameboard.js
@@ -1,5 +1,23 @@
+import Square from '../square/square.js';
+
 export default class Gameboard{
     constructor() {
-        this.array = [];
+        this.array = this.buildBoard();
+    }
+
+    buildBoard() {
+        const board = [];
+        for(let i = 0; i < 10; i++) {
+            for(let j = 0; j < 10; j++) {
+                const square = new Square([i, j]);
+
+                board.push(square);
+            }
+        }
+
+        return board;
     }
 }
+
+const gameboard = new Gameboard();
+gameboard.array;
