5697aef448eb1290fcd6a7130cef849184e64d83
James Olsen <olsenjames1116@gmail.com>
2023-02-13 18:41:25 -0700

    Gameboard has an array property

--- src/gameboard/gameboard.js
+++ src/gameboard/gameboard.js
@@ -1,3 +1,5 @@
 export default class Gameboard{
-    
+    constructor() {
+        this.array = [];
+    }
 }
--- src/gameboard/gameboard.test.js
+++ src/gameboard/gameboard.test.js
@@ -5,3 +5,7 @@ const gameboard = new Gameboard();
 test('Returns an object', () => {
     expect(typeof gameboard).toBe('object');
 });
+
+test('Gameboard has an array property', () => {
+    expect(Array.isArray(gameboard.array)).toBe(true);
+});
