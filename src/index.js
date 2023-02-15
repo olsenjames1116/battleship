6e0b2409cc42adf617aab0869929c9161c6d533c
James Olsen <olsenjames1116@gmail.com>
2023-02-15 14:53:08 -0700

    Get index of clicked square
    The index of a grid square is grabbed when that square is clicked. The
    Nodelist of elements needed to be converted to an array to achieve this.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import Game from './game/game.js';
 const game = new Game();
 game.playGame();
 
-const computerGrid = document.querySelectorAll('div#computer>div.grid');
+const computerGrid = Array.from(document.querySelectorAll('div#computer>div.grid'));
 computerGrid.forEach((grid) => {
-    grid.addEventListener('click', () => {
-        console.log('grid click');
+    grid.addEventListener('click', (event) => {
+        console.log(computerGrid.indexOf(event.target));
     });
 });
