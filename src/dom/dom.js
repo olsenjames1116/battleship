f560081df58d0e7e8906190eebabfadc2c687ddc
James Olsen <olsenjames1116@gmail.com>
2023-02-15 14:39:32 -0700

    Add event listener to computer grid
    Event listeners were added to each grid square in the computer's board.
    This will be used to track moves made by the user.

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -1,23 +1,24 @@
 export default class Dom {
+    createGrids(board) {
+        for(let i = 0; i<100; i++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.classList.add('grid');
+            board.appendChild(gridSquare);
+        }
+    }
+
     loadBoards() {
         const playerBoard = document.createElement('div');
         playerBoard.classList.add('gameboard');
+        playerBoard.id = 'player';
         const computerBoard = document.createElement('div');
         computerBoard.classList.add('gameboard');
+        computerBoard.id = 'computer';
         
         const content = document.querySelector('div.content');
         content.append(playerBoard, computerBoard);
 
-        for(let i = 0; i<100; i++) {
-            const gridSquare = document.createElement('div');
-            gridSquare.classList.add('grid');
-            playerBoard.appendChild(gridSquare);
-        }
-
-        for(let i = 0; i < 100; i++) {
-            const gridSquare = document.createElement('div');
-            gridSquare.classList.add('grid');
-            computerBoard.appendChild(gridSquare);
-        }
+        this.createGrids(playerBoard);
+        this.createGrids(computerBoard);
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import Game from './game/game.js';
 
 const game = new Game();
 game.playGame();
+
+const computerGrid = document.querySelectorAll('div#computer>div.grid');
+computerGrid.forEach((grid) => {
+    grid.addEventListener('click', () => {
+        console.log('grid click');
+    });
+});
