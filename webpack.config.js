87b45680148166b60b8e258872dc910bc8dde5bd
James Olsen <olsenjames1116@gmail.com>
2023-02-17 17:57:15 -0700

    Add loaders for images and fonts to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,14 @@ module: {
             ]
           }
         }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       }
     ],
 },
